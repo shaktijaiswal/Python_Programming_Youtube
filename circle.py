4a8821881b9dbf2eb7367c45b33de94fcd8cdbd6
codejay411 <jaypr202@gmail.com>
2020-08-23 16:39:45 +0530

    Circle commit

---
+++ circle.py
@@ -0,0 +1,14 @@
+# Area of circle
+# Circumference of circle
+radius=float(input("Enter the radius of circle : "))
+area=3.14*radius*radius
+
+# Area of circle
+print("Area of circle: ", area)
+
+# circumference of circle
+circum=2*3.14*radius
+print("Circumference of circle: ", circum)
+
+
+# rectangle, square, cylinder, sphere etc
